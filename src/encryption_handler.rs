6848c0ba94479e616cf497c0a1d6c2d52411f274
Di Saber <marc.a.benamou@gmail.com>
2023-07-22 20:12:33 -0500

    Increased the password hash iterations by a factor of 10

--- src/encryption_handler.rs
+++ src/encryption_handler.rs
@@ -9,7 +9,7 @@ use sha3::{Digest, Sha3_256};
 
 use crate::data_container::DataContainer;
 
-const RECOMMENDED_HASH_ITERATIONS: i32 = 10000;
+const RECOMMENDED_HASH_ITERATIONS: i32 = 100000;
 
 pub fn from_encrypted<T>(
     cipher_text: &str,
