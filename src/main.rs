84894a573e87a9ce5267c5649fad35dabc2363a0
Di Saber <marc.a.benamou@gmail.com>
2023-08-01 07:48:01 -0500

    Remove old console comments

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,6 @@ use std::{fs, path::Path};
 
 use utils::VERSION_CODE;
 
-/*
-main_window.clear();
-main_window.begin();
-
-// New window
-
-main_window.end();
-main_window.redraw();
- */
-
 fn main() {
     let data_path = ProjectDirs::from("com", "DiSaber", "BlockCypher").unwrap();
     let data_path: &Path = data_path.config_dir();
@@ -47,39 +37,4 @@ fn main() {
     };
 
     app.run().unwrap();
-
-    /*loop {
-        term.clear_screen().unwrap();
-        println!(
-            "BlockCypher {VERSION_CODE}
-
-            \rPress the {} key to encrypt a message
-            \rPress the {} key to decrypt a recieved message
-            \rPress the {} key to enter the contacts menu
-
-            \rPress the {} key to change your password
-
-
-            \rPress the {} key to terminate your session",
-            style("'e'").cyan(),
-            style("'d'").cyan(),
-            style("'c'").cyan(),
-            style("'p'").cyan(),
-            style("escape").red()
-        );
-
-        let key = term.read_key().unwrap_or(Key::Unknown);
-
-        match key {
-            Key::Char('e') => screens::cipher::encrypt(&term, &mut clipboard, &program_data),
-            Key::Char('d') => screens::cipher::decrypt(&term, &program_data),
-            Key::Char('c') => screens::contacts::contacts(&term, &mut clipboard, &mut program_data),
-            Key::Char('p') => screens::auth::change(&term, &mut program_data),
-            Key::Escape => {
-                term.clear_screen().unwrap();
-                return;
-            }
-            _ => (),
-        };
-    }*/
 }
